d9c790257597ab2e703a9bd3e10281ef59f5e4c7
Duncan Temple Lang <duncan@r-project.org>
2020-01-24 08:11:23 -0800

    moved the function definitions from TOY2.R to functions.R. Vectorize() calls still in TOY2.R

--- TOY2.R
+++ TOY2.R
@@ -47,9 +47,6 @@ rm(list=ls(all=TRUE)) #start with empty workspace
 setwd("C:/Users/leadams/Desktop/ShastaDam/DP")
 getwd()
 ####c.global functions
-mround <- function(x,base){ #this function ensures matching between stages
-  base*round(x/base) 
-} 
 monthlist=vector()
 for(i in 1:12){ #gets month
   monthlist[i]=month.name[i]
@@ -81,24 +78,7 @@ Vw=seq(0,Max, bin) #sequence of Vw choices
 V=Vc+Vw
 L=seq(-0.5*bin, Max+0.5*bin, bin) #makes bins 
 ###state space
-Vdiscretizations=function(Vc,Vw){
-  i=j=1
-  z=0
-  Vcoptions=length(Vc)
-  Vwoptions=length(Vw)
-  Voptions=matrix(0, nrow=3, ncol=Vcoptions*Vwoptions)
-  for(i in 1:Vcoptions){
-    for (j in 1:Vwoptions){
-      VH=Vc[i]+Vw[j]
-      z=z+1
-      Voptions[1,z]=Vw[j]
-      Voptions[2,z]=Vc[i]
-      Voptions[3,z]=VH
-    }
-  }
-  rownames(Voptions)=c("Vw","Vc", "V")
-  return(Voptions)
-}
+
 Voptions=t(Vdiscretizations(Vc,Vw)[c(1:2),])
 colnames(Voptions)=c("Vc","Vw")
 #apprximated states (vector)
@@ -149,15 +129,7 @@ for(i in pn:1){ #calculates deltaTa for the first observation
 deltaTa=mround(deltaTa,0.1) #uses mround function from global functions in this script
 
 ######monthly inflow
-finalinflowprep=function(month,Q){ #converts from cfs to taf
-  monthlyQ=ifelse(month == "January"|| month=="March"|| month=="June"|| month=="July"||month=="August"||month=="October"||month=="December", 
-                  Q*1.98*31, #Q*cfs to af* day number in month
-                  ifelse(month=="February", 
-                         Q*1.98*28,
-                         #as.numeric(Lookupy[which(Lookupy[,1]==month),3])*1.98*28,
-                         Q*1.98*30))
-  return(monthlyQ)
-}
+
 finalinflow=Vectorize(finalinflowprep)
 rawQ=climate[,6]
 Q=mround(finalinflow(climate[,2],rawQ), bin)
@@ -168,17 +140,9 @@ ystates=probs
 Lookupyprep=cbind(climate,Q,deltaTa)
 Lookupy=Lookupyprep[,c(2,10,11,12)]
 
-TaLookupprep=function(month,p){
-  Ta=Lookupy[which(Lookupy[,1]==month & Lookupy[,2]==p),4] #the max is just in case there are multiples 
-  return(Ta)
-}
 TaLookup=Vectorize(TaLookupprep)
 
-QLookupprep=function(month,p){ #this could be VC, Vc or Vcstates
-  Qprep=Lookupy[which(Lookupy[,1]==month & Lookupy[,2]==p),3]
-  Q=as.numeric(Qprep)
-  return(Q)
-}    
+
 QLookup=Vectorize(QLookupprep)
 #########
 #######ii. T|V
@@ -209,31 +173,9 @@ ObservedLookupTable=cbind(observedVc,ObservedTc,observedVw,ObservedTw)
 #######iii. season characteristics|month|Stage
 ###############
 #gets month based on stage number
-monthcounter=function(Stage){ #gives month per stage number
-  monthlocation=ifelse(Stage%%12==0, 12, Stage - floor(Stage/12)*12)
-  month=month.name[monthlocation] #starts with january
-  return(month)
-}
+
 #gets climate season based on month
-seasonbin=function(month){
-  season=ifelse(month== "December" || month== "January", "winter", 
-                ifelse(month== "February" || month== "March", "earlyspring", 
-                       ifelse(month== "April", "spring", 
-                              ifelse(month== "May" || month=="June" || month== "July", "summer", 
-                                     ifelse(month== "August" || month== "September" || month=="October", "latesummer",
-                                            ifelse(month== "November", "fall", 
-                                                   "ERROR"))))))
-  return(season)
-}
-#determines if lake is stratified or not based on month
-lakeseasonbin=function(month){
-  lakeseason=ifelse(month== "December" || month== "January", "winter", 
-                    ifelse(month== "February" || month== "March", "earlyspring", 
-                           ifelse(month== "April" || month== "May" || month== "June" || month== "July" ||month== "August" || month== "September" || month=="October", "stratified",
-                                  ifelse(month== "November", "overturn", 
-                                         "ERROR"))))
-  return(lakeseason)
-}
+
 
 #####################
 #####-finding max Qc and Qw to include spill
@@ -252,27 +194,11 @@ afconversion=810714 #AF
 springcoeff=NickelsSpringCoefficients*c(afconversion,1, afconversion,1) 
 wintercoeff=NickelsWinterCoefficients*c(afconversion,afconversion,1,afconversion) 
 
-WinterDeltaVcprep=function(month,p){ 
-  DeltaVc=mround(wintercoeff[1]+wintercoeff[2]*as.numeric(TaLookup(month,p))+wintercoeff[3]*as.numeric(QLookup(month,p))+wintercoeff[4]*1.6, bin) 
-  #round((0.855*Qin+0.264*TcLookup(VC,VW)+(0.253*0.12)*Qin+0.887)/2, digits=-6) #check eq and units
-  return(DeltaVc)
-}
 WinterDeltaVc=Vectorize(WinterDeltaVcprep)
 
-SpringDeltaVcprep=function(RcstarWinter,month, RC,p){
-  DeltaVc=ifelse((springcoeff[1]+springcoeff[2]*RC+springcoeff[3]*as.numeric(TaLookup(month,p))+springcoeff[4]*as.numeric(QLookup(month,p))) <0,0,
-                 (springcoeff[1]+springcoeff[2]*RC+springcoeff[3]*as.numeric(TaLookup(month,p))+springcoeff[4]*as.numeric(QLookup(month,p))))
-  AdjustedforRcWinter=mround(DeltaVc#-1.5*10^6
-                             ,bin)
-  return(AdjustedforRcWinter)
-}
+
 SpringDeltaVc=Vectorize(SpringDeltaVcprep)
 
-ColdDeltaprep=function(month, RcstarWinter,RC,p){ 
-  DeltaVc=ifelse(lakeseasonbin(month)=="winter",WinterDeltaVc(month,p),
-                 SpringDeltaVc(RcstarWinter,month,RC,p)) 
-  return(DeltaVc)
-}
 ColdDelta=Vectorize(ColdDeltaprep)
 
 #####################
@@ -280,26 +206,6 @@ ColdDelta=Vectorize(ColdDeltaprep)
 #####################
 
 #get discretizations with spill (remember to line up the index)
-VRdiscretizations=function(VC,VW,RC,RW){
-  i=j=k=l=1
-  z=0
-  allVRoptions=matrix(0, nrow=length(VC)*length(VW)*length(RC)*length(RW), ncol=4)
-  for(i in 1:length(RW)){
-    for (j in 1:length(RC)){
-      for(k in 1:length(VW)){
-        for(l in 1:length(VC)){
-          z=z+1
-          allVRoptions[z,1]=VC[l]
-          allVRoptions[z,2]=VW[k]
-          allVRoptions[z,3]=RC[j]
-          allVRoptions[z,4]=RW[i]
-        }
-      }
-    }
-  }
-  colnames(allVRoptions)=c("Vc","Vw", "Rc","Rw")
-  return(allVRoptions)
-}
 #get max available VC during fall overturn #calculations from fallsolve
 MaxQcprep=max(ColdDelta("January",RcstarWinter, nospillRc, p),
               ColdDelta("February",RcstarWinter, nospillRc, p),
@@ -324,14 +230,6 @@ AvailVw=seq(0,MaxQw+max(Vw),bin) #vc and Vw includes inflow and atm conditions
 ########make V| T to include start and end of period storage possibilities (excluding releas options)
 #aggregated into uniform bins of width "bin" for DP 
 
-MakingBins=function(ObservedLookupTable,observedVc,observedVw,Vc,Vw,L){
-  Vcintervalbin=cut(as.numeric(observedVc),breaks=L, labels=Vc)
-  Vwintervalbin=cut(as.numeric(observedVw),breaks=L, labels=Vw)
-  allbinned=cbind(as.numeric(as.character(Vcintervalbin)),as.numeric(as.character(Vwintervalbin)),ObservedLookupTable)
-  colnames(allbinned)[[1]]=c("Vcbin") 
-  colnames(allbinned)[[2]]=c("Vwbin") 
-  return(allbinned)
-}
 
 allbins=MakingBins(ObservedLookupTable,observedVc,observedVw,Vc,Vw,L)
 preppeddata=allbins
@@ -375,20 +273,6 @@ LookupTable=LookupTableprep3[order(LookupTableprep3[,1],LookupTableprep3[,2],Loo
 greaterTc=LookupTable[LookupTable[,1]==max(Vc) & LookupTable[,2]==0,3]
 greaterTw=LookupTable[LookupTable[,1]==0 & LookupTable[,2]==max(Vw),4]
 ##########get T from R and V
-ReleaseTempprep=function(VC,VW, RC, RW){
- # Tc=LookupVRTprep[(LookupVRTprep[,1]==VC) & (LookupVRTprep[,2]==VW) & (LookupVRTprep[,3]==RC) & (LookupVRTprep[,4]==RW),5]
-#  Tw=LookupVRTprep[(LookupVRTprep[,1]==VC) & (LookupVRTprep[,2]==VW) & (LookupVRTprep[,3]==RC) & (LookupVRTprep[,4]==RW),6] 
-  Tc=ifelse(VC>max(Vc), greaterTc,
-            ifelse(VW>max(Vw), LookupTable[(LookupTable[,1]==VC) & (LookupTable[,2]==max(Vw)),3],
-            LookupTable[(LookupTable[,1]==VC) & (LookupTable[,2]==VW),3]))
-  Tw=ifelse(VW>max(Vw), greaterTw,
-            ifelse(VC>max(Vc), LookupTable[(LookupTable[,1]==max(Vc)) & (LookupTable[,2]==VW),4],
-            LookupTable[(LookupTable[,1]==VC) & (LookupTable[,2]==VW),4]))
-  T=ifelse(Tc==0, Tw,
-           ifelse(Tw==0, Tc,
-                  (Tw*RW+Tc*RC)/(RC+RW)))
-  return(T)
-} 
 ReleaseTemp=Vectorize(ReleaseTempprep)
 
 ###plot RC and RW and TC and TW
@@ -411,28 +295,7 @@ Rw=seq(0,MaxQw+max(nospillRw),bin)
 #get x from T
 
 ##consistent monthly target of 56F
-ClrCk=function(RT){
-  x=ifelse(2.67491 + 0.95678*RT <30,0,
-             2.67491 + 0.95678*RT)
-  return(x)
-}
-Balls=function(RT){
-  x=ifelse(11.26688 + 0.81206*RT<30,0,
-             11.26688 + 0.81206*RT)
-  return(x)
-}
-Jelly=function(RT){
-  x=ifelse(7.74007 + 0.88836*RT <30, 0,
-       7.74007 + 0.88836*RT)
-  return(x)
-}
-#Bend=ifelse(31.69574 + 0.41228*LookupVRT$ReleaseT <33, 0, #additional Bend influences?
- #     31.69574 + 0.41228*LookupVRT$ReleaseT)
-RBDD=function(RT){
-  x=ifelse(15.8956 + 0.7529*RT <30, 0,
-      15.8956 + 0.7529*RT)
-  return(x)
-}
+
 
 #Lookupx=cbind(ClrCk,Balls,Jelly)#,#Bend,
                     #RBDD)
@@ -462,14 +325,6 @@ EmbryoIncubation=55
 Emergents=68
 OutmigratingJuveniles=66
 
-fishtemp=function(month){
-  temp=ifelse(month=="March"|| month=="April",ReturningAdults,
-              ifelse(month=="May" || month=="June" || month=="July", EmbryoIncubation,
-                     ifelse(month=="August", Emergents,
-                            OutmigratingJuveniles)
-              ))
-  return(temp)
-}
 
 #LookupVRTx=cbind(LookupVRT,ConstantTempx)
  #plot(ReleaseT,ConstantTempx)
@@ -520,15 +375,7 @@ Rspace=t(apply(basics,1,function(x)Rdecs))
 #outcome spaces (matrices)
 ##creates matrix for holding each stage calculation
 #can include y
-stagepolicy=function(VSpace,NoofStages){
-  holdingmatrix=matrix(0, nrow=length(Vstates), #ncol=length(ystates)*NoofStages) 
-                       ncol=NoofStages)
-  dim(holdingmatrix)=c(length(Vstates),#length(ystates), 
-                       NoofStages)
-  rownames(holdingmatrix)=Vstates
-  #colnames(holdingmatrix)=ystates
-  return(holdingmatrix)
-}
+
 fstar=stagepolicy(Vstates,NoofStages)
 #whichf    #could possible shorten code by saying whichxstar=f, for all below options
 whichxstar=stagepolicy(Vstates,NoofStages)
@@ -541,24 +388,9 @@ Rwstar=stagepolicy(Vstates,NoofStages)
 #############
 ####d. end of period storage |(R,V,y)
 ############
-OutgoingVcprep=function(S,VC, RcstarWinter,VW,RC,RW,p){ #put month in quotations, add O #all matrices
-  month=monthcounter(S)
-  endperiodVc=ifelse(lakeseasonbin(month)=="winter", VC+WinterDeltaVc(month,p)-RC,
-                     ifelse(lakeseasonbin(month)=="earlyspring", VC+SpringDeltaVc(RcstarWinter,month, RC,p)-RC, 
-                            ifelse(lakeseasonbin(month)=="stratified", VC-RC, 
-                                   ifelse(lakeseasonbin(month)=="overturn", VC+QLookup(month,p)-RC+VW-RW,
-                                          "ERROR"))))
-  return(endperiodVc)
-}
+
 OutgoingVc=Vectorize(OutgoingVcprep)
-OutgoingVwprep=function(S,VW,RW,p){ #put month in quotations
-  month=monthcounter(S)
-  Vwnext=ifelse(lakeseasonbin(month)=="winter" || lakeseasonbin(month)=="earlyspring" ||lakeseasonbin(month)=="overturn",0,
-                ifelse(lakeseasonbin(month)=="stratified", VW+QLookup(month,p)-RW,#+E 
-                       #ifelse(lakeseasonbin(month)=="overturn", Vw-Rw, 
-                       "ERROR"))
-  return(Vwnext)
-}
+
 OutgoingVw=Vectorize(OutgoingVwprep)
 
 ##############
@@ -568,115 +400,26 @@ OutgoingVw=Vectorize(OutgoingVwprep)
 ######
 ####a. direct benefit function
 ############
-benefitprep=function(Vc,Vw,Rc,Rw,month){
-  RT=ReleaseTemp(Vc,Vw,Rc,Rw)
-  #xrow=LookupVRTx[which(LookupVRTx[,1]==Vc & LookupVRTx[,2]==Vw & LookupVRTx[,3]==Rc & LookupVRTx[,4]==Rw),]
-  tempthreshold=fishtemp(month)
-  x=ifelse(RT==0, 0, #if release temp is 0
-                       #ifelse(xrow$RBDD<tempthreshold,distance[6], 
-                             # ifelse(xrow$Bend<tempthreshold,distance[6],
-                                     ifelse(Jelly(RT)<tempthreshold,distance[5],   
-                                            ifelse(Balls(RT)<tempthreshold, distance[4],
-                                                   ifelse(ClrCk(RT)<tempthreshold, distance[3],
-                                                          0))))#)#)
-  
-  return(x)
-}
+
 benefit=Vectorize(benefitprep)
 
 ####each lake and climate season's benefits | end of month storage
-mixedsolveprep=function(VW,VC,RC, RW, R, V, month, RcstarWinter, K, DP,p){ #when lake is mixed #matrices
-  deltaVC=#ifelse(month=="February" || month=="March", deltaVC-springcoeff[2]*RC, WinterDeltaVc(month,p))
-         ColdDelta(month, RcstarWinter,RC,p)
-  AvailableVc=ifelse(VC+deltaVC<0,0,VC+deltaVC)
-  x=ifelse(VW>0 || RW>0, -9999, #no warmpool
-           ifelse(VC +deltaVC < RC, -9999, #consv of mass (not enough VC) 
-                  ifelse(V + deltaVC- R < DP || 
-                           V + deltaVC - R > K, -9999, #infrastructure limitations
-                         benefit(AvailableVc,VW,RC,RW,month) #ifelse(Nmax< x, Nmax, x)
-                  )))
-  return(x)
-}
+
 mixedsolve=Vectorize(mixedsolveprep)
-springsolveprep=function(VW,VC,RC, RW, R, V,K, DP,month,p){ #initial conditions are no warm 
-  deltaVw=QLookup(month,p)
-  AvailableVw=ifelse(VW+deltaVw <0, 0,
-                     #ifelse(Vw+deltaVw >max(VW), max(VW),
-                            Vw+deltaVw)#)
-  x=ifelse(VW>0, -9999, #no warmpool at start, all warm comes from inflows
-    ifelse(V + deltaVw - R < DP | V + deltaVw - R > K, -9999, #inflow is warm, cold stays, #infrastructure limitations
-           ifelse(VC < RC | VW + deltaVw < RW, -9999, #consv of mass
-                  benefit(VC,AvailableVw,RC,RW,month)  #ifelse(Nmax< x, Nmax, x)
-           )))
-  return(x)
-}
+
 springsolve=Vectorize(springsolveprep)
-summersolveprep=function(VW,VC,RC, RW, R, V,K, DP,month,p){ #initial conditions are no warm (i dont like this, want VW to organically come online)
-  deltaVw=QLookup(month,p)
-  AvailableVW=ifelse(VW+deltaVw<0, 0, VW+deltaVw)
-  x= ifelse(V + deltaVw - R < DP | V + deltaVw- R > K, -9999, #infrastructure limitations
-            ifelse(VC < RC, -9999, #consv mass
-                   ifelse(VW + deltaVw < RW, -9999,
-                          benefit(VC,AvailableVW,RC,RW,month)  #ifelse(Nmax< x, Nmax, x)
-                   )))
-  return(x)
-}
+
 summersolve=Vectorize(summersolveprep)
-fallsolveprep=function(VW,VC,RC,RW,K,DP,month,p){ #initial conditions are no warm (i dont like this, want VW to organically come online)
-  deltaVc=QLookup(month,p)+VW-RW
-  AvailableVC=ifelse(VC+deltaVc<0, 0, VC+deltaVc)
-  x=ifelse(#RW > 0, -9999, #all cold at the end
-    VC + deltaVc - RC < DP || VC + deltaVc - RC > K, -9999, #infrastructure limitations, no spill allowed
-    ifelse(VC+deltaVc < RC, -9999, #all water becomes cold #|| VW < RW, -9999, #consv mass
-           ifelse(VW<RW,-9999,
-                  benefit(AvailableVC,VW,RC,RW,month)  #ifelse(Nmax< x, Nmax, x)
-           )))
-  return(x)
-}
+
 fallsolve=Vectorize(fallsolveprep)
 
 #objective function/calculate current benefits
-choosesolveprep=function(month,VW,VC,RC, RW, R, V,RcstarWinter,K, DP,p){ #matrix
-  x=ifelse(seasonbin(month)=="winter" || seasonbin(month)=="earlyspring", mixedsolve(VW,VC,RC, RW, R, V, month, RcstarWinter,K, DP,p),
-           ifelse(seasonbin(month)=="spring", springsolve(VW,VC,RC, RW, R, V,K, DP,month,p),
-                  ifelse(seasonbin(month)=="summer" || seasonbin(month)=="latesummer" , summersolve(VW,VC,RC, RW, R, V,K, DP,month,p),
-                         #ifelse(seasonbin(month)=="october", octobersolve(VW,VC,RC, RW, R, V,K, DP,month,p),
-                           ifelse(seasonbin(month)=="fall", fallsolve(VW,VC,RC, RW, K, DP,month,p),
-                                "ERROR"))))#)
-  return(x)
-} 
+
 choosesolve=Vectorize(choosesolveprep)
 
 ###############
 ####b. accumulative obj function
 #######################
-accumulate=function(month,S,Vcstates,Vwstates,VcSpace,RcstarWinter,VwSpace,Rcspace,Rwspace,VSpace,Rspace,p){  #lookup f*t+1 
-  for(j in 1:length(Rdecs)){ 
-    for(i in 1:length(Vstates)){ 
-      fs[i,j]=ifelse(choosesolve(month,VwSpace[i,j],VcSpace[i,j],Rcspace[i,j], Rwspace[i,j], Rspace[i,j],VSpace[i,j],RcstarWinter,K, DP,p)<0,-9999, #remove infeasibles
-                     which(Vcstates==OutgoingVc(S,VcSpace[i,j],RcstarWinter,VwSpace[i,j],Rcspace[i,j],Rwspace[i,j],p) & #,Vcstates)[i,j] & #this gives the location of Vcstates
-                             Vwstates==OutgoingVw(S, VwSpace[i,j], Rwspace[i,j],p))) #match Vw and Vc in LookupV table #location of Vw states
-      fstarvalue[i,j]=ifelse(is.na(fs[i,j]), -9999, ifelse(fs[i,j]<0, -9999, #remove infeasibles
-                                                           fstar[fs[i,j],(S+1)])) #get the fstar from t+1 with the matching Vw and Vc states
-    } 
-  } 
-  return(fstarvalue) #produces a matrix of fstartt+1 values to accumulate in the benefit function, looking backwards
-}
-
-firststageaccumulate=function(month, S, Vcstates,Vwstates,Vcinitial,RcstarWinter,Vwinitial,Rcdecs,Rwdecs,Rdecs, Vinitial,p){
-  for(j in 1:length(Rdecs)){#calculates f*t+1 from next stage
-    fs[j]=ifelse(choosesolve(month,Vwinitial,Vcinitial,Rcdecs[j], Rwdecs[j], Rdecs[j],Vinitial,RcstarWinter,K, DP,p)<0,-9999, #remove infeasibles
-                which(Vcstates==OutgoingVc(S,Vcinitial,RcstarWinter,Vwinitial,Rcdecs[j],Rwdecs[j],p) & #,Vcstates)[i,j] & 
-                        Vwstates==OutgoingVw(S, Vwinitial, Rwdecs[j],p))) 
-                #which(Vcstates==OutgoingVc(S,Vcinitial,RcstarWinter,Vwinitial,Rcdecs[j],Rwdecs[j],p) & #,Vcstates)[i,j] & 
-                 #        Vwstates==OutgoingVw(S, Vwinitial, Rwdecs[j],p))) #match Vw and Vc in LookupV table
- 
-# }
-    fstarvalue[j]=ifelse(is.na(fs[j]), -9999, ifelse(fs[j]<0, -9999, fstar[fs[j],(S+1)])) #get the fstar from t+1 with the matching Vw and Vc states
- #   print(test)
-    }
-  return(fstarvalue) #produces a matrix of fstartt+1 values to accumulate in the benefit function, looking backwards
-}
 
 #######
 ##DP model
