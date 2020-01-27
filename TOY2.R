c556dd84b2863fdbb48e74c55cc6b01952563f72
Duncan Temple Lang <duncan@r-project.org>
2020-01-26 19:11:23 -0800

    indent 2 lines of comments in TOY2.R

--- TOY2.R
+++ TOY2.R
@@ -154,8 +154,8 @@ rm(tmp.m, tmp.p)
 
 
 if(UseInline) {
-# Inline the values of LookupyQ and LookupyTa into QLookup and TaLookup body's expression.
-# Assumes both functions have no {} around the body, but each body is just a single call.
+     # Inline the values of LookupyQ and LookupyTa into QLookup and TaLookup body's expression.
+     # Assumes both functions have no {} around the body, but each body is just a single call.
     b = body(QLookup)
     if(is.symbol(b[[2]])) {
         b[[2]] = LookupyQ
